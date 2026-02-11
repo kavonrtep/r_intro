c9100f8be9c6d9496aa3b6c35d2e2c602aa7082c
Petr Novak <petr@umbr.cas.cz>
2026-02-11 20:05:36 +0100

    index and data added

---
+++ 09_Biostrings/data/seq_data/get_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://ftp.ensembl.org/pub/release-113/fasta/danio_rerio/dna/Danio_rerio.GRCz11.dna_sm.toplevel.fa.gz
+wget https://ftp.ensembl.org/pub/release-113/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna_sm.toplevel.fa.gz
